a0fd11cc100149269f3372fcadf2152e1a638efe
Pat Christopher <patrick.christopher@mtnsat.com>
2014-08-20 10:29:08 -0700

    hey yo, it works now

--- create-bats-timepairs.go
+++ create-bats-timepairs.go
@@ -3,6 +3,7 @@ package main
 import (
   "os"
   "fmt"
+  "log"
   "sort"
   "flag"
   "time"
@@ -11,15 +12,131 @@ import (
   ioutil "io/ioutil"
   json "encoding/json"
   http "net/http"
+  amqp "github.com/streadway/amqp"
 )
 
 var (
-  eshost = "http://kibana.mtnsatcloud.com:9200/logstash-2014.07*,logstash-2014.08.0*,logstash-2014.08.10,logstash-2014.08.11,logstash-2014.08.12/_search?fields=@timestamp,event,shipName"
-  esquery_ur = "&q=@type:zeroimpact-rabbitmq-input%20and%20type:event%20and%20event:Switching*%20and%20shipName:Coral"
-  esquery_doc = `{"query":{"filtered":{"query":{"bool":{"must":[{"query_string":{"query":"Switching"}}]}},"filter":{"bool":{"must":[{"range":{"@timestamp":{"from":1407794970791,"to":1407967770791}}},{"fquery":{"query":{"query_string":{"query":"@type:zeroimpact-rabbitmq-input"}}}},{"fquery":{"query":{"query_string":{"query":"type:event or rmevent"}}}},{"fquery":{"query":{"query_string":{"query":"shipName:PROVIDEPYLONS"}}}}]}}}},"size":500,"sort":[{"@timestamp":{"order":"asc","ignore_unmapped":true}},{"@timestamp":{"order":"asc","ignore_unmapped":true}}]}`
+  eshost = "http://kibana.mtnsatcloud.com:9200/logstash-PROVIDEPYLONS/_search?fields=@timestamp,event,shipName"
+  esindexdate = flag.String("indexdate", "", "date to query elasticsearch")
+  esquery_doc = `{"query":{"filtered":{"query":{"bool":{"must":[{"query_string":{"query":"Switching"}}]}},"filter":{"bool":{"must":[{"fquery":{"query":{"query_string":{"query":"@type:zeroimpact-rabbitmq-input"}}}},{"fquery":{"query":{"query_string":{"query":"type:event or rmevent"}}}},{"fquery":{"query":{"query_string":{"query":"shipName:PROVIDEPYLONS"}}}}]}}}},"size":500}`
   shipname = flag.String("shipname", "", "name of ship according to value set by intelligent router")
   spool_dir = flag.String("spool_dir", "/mnt/spooled_messages", "name of ship according to value set by intelligent router")
+  uri          = flag.String("uri", "amqp://datahouse:d4t4m4ngl3@localhost:5672/%2Fdatahouse", "AMQP URI")
+  exchange     = flag.String("exchange", "netflow_dummy", "Durable AMQP exchange name")
+  exchangeType = flag.String("exchangeType", "direct", "Exchange type - direct|fanout|topic|x-custom")
+  routingKey   = flag.String("routingKey", "netflow_dummy", "AMQP routing key")
 )
+
+type RabbitChanWriter struct {
+  amqpchan *amqp.Channel
+  amqpconn *amqp.Connection
+}
+
+func (rcw *RabbitChanWriter) Close() (err error) {
+  return rcw.amqpconn.Close()
+}
+
+func (rcw *RabbitChanWriter) Write(b []byte) (n int, err error) {
+  if err = rcw.amqpchan.Publish(
+    *exchange,
+    *routingKey,
+    false,
+    false,
+    amqp.Publishing{
+      Headers:         amqp.Table{},
+      ContentType:     "text/plain",
+      ContentEncoding: "",
+      Body:            b[0:len(b)],
+      DeliveryMode:    amqp.Persistent,
+    },
+  ); err != nil {
+    log.Printf("Exchange Publish: %s", err)
+    return -1, err
+  }
+
+  return len(b), nil
+}
+
+
+/*
+**  build the connection to the rabbitmq server
+*/
+func buildChannel() (rChan *RabbitChanWriter) {
+  connection, err := amqp.Dial(*uri)
+  if err != nil {
+    log.Printf("Dial: %s", err)
+    return nil
+  }
+
+  channel, err := connection.Channel()
+  if err != nil {
+    log.Printf("Channel: %s", err)
+    return nil
+  }
+
+  //  build the exchange
+  if err := channel.ExchangeDeclare(
+    *exchange,     // name
+    *exchangeType, // type
+    true,         // durable
+    false,        // auto-deleted
+    false,        // internal
+    false,        // noWait
+    nil,          // arguments
+  ); err != nil {
+    log.Fatalf("Exchange Declare: %s", err)
+  }
+
+  //  create a queue with the routing key and bind to it
+  if _, err := channel.QueueDeclare(
+    *routingKey,    //  name
+    true,           //  durable
+    false,          //  autoDelete
+    false,          //  exclusive
+    false,          //  noWait
+    nil,            //  args
+  ); err != nil {
+    log.Fatalf("Queue Declare: %s", err)
+  }
+
+  if err := channel.QueueBind(
+    *routingKey,    //  name
+    *routingKey,    //  key
+    *exchange,      //  exchange
+    false,          //  noWait
+    nil,            //  args
+  ); err != nil {
+    log.Fatalf("Queue Bind: %s", err)
+  }
+
+
+  rChan = &RabbitChanWriter{channel, connection}
+  return
+}
+
+
+/*
+**  writes a message on the internal 'messages' chan to rabbitmq
+*/
+func writeToRabbit(rabbitWriter *RabbitChanWriter, message []byte){
+  _, err := rabbitWriter.Write(message)
+  if err != nil {
+    log.Printf("got an error of: %s", err)
+    //  close it up, have a nap, try again
+    rabbitWriter.Close()
+    time.Sleep(1 * time.Second)
+    for {
+      rabbitWriter = buildChannel()
+      if rabbitWriter != nil {
+        break
+      } else {
+        log.Println("failed to connect to rabbitmq server, nap and try again")
+        time.Sleep(10*time.Second)
+      }
+    }
+  }
+}
+
 
 type ByTime []Hits
 
@@ -34,11 +151,13 @@ type kbResp struct {
 }
 
 type Hits struct {
-  Fields struct{
-    Timestamp []string `json:"@timestamp"`
-    Event []string `json:"event"`
-    ShipName []string `json:"shipName"`
-  } `json:'fields'`
+  Fields Fields `json:'fields'`
+}
+
+type Fields struct{
+  Timestamp []string `json:"@timestamp"`
+  Event []string `json:"event"`
+  ShipName []string `json:"shipName"`
 }
 
 type BatsRange struct {
@@ -55,37 +174,47 @@ func (b BatsRange) ToString() {
   fmt.Println("Start time is: ", b.StartTime, " and End time is: ", b.EndTime)
 }
 
-func createBatsTimeline(shipname string) (BatsRanges []BatsRange) {
+func createBatsTimeline(shipname, esindexdate string) (BatsRanges []BatsRange) {
   //  build a client
   client := &http.Client{}
-  resolved_esquery_doc := strings.Replace(esquery_doc, "PROVIDEPYLONS",shipname, 1)
+  resolved_esquery_doc := strings.Replace(esquery_doc, "PROVIDEPYLONS", shipname, 1)
+  resolved_eshost := strings.Replace(eshost, "PROVIDEPYLONS", esindexdate, 1)
   readeresdoc := bytes.NewBufferString(resolved_esquery_doc)
-  seriesReq, err := http.NewRequest("GET", eshost, readeresdoc)
+  seriesReq, err := http.NewRequest("GET", resolved_eshost, readeresdoc)
   seriesReq.Header.Add("Content-Type", "Application/json")
 
   resp, err := client.Do(seriesReq)
   if err != nil {
-    fmt.Println(err)
+    log.Println(err)
   } else {
     var kibanaRespons kbResp
     decoder := json.NewDecoder(resp.Body)
     err = decoder.Decode(&kibanaRespons)
     if err != nil {
-      fmt.Println("Failed get!")
-        fmt.Println(err)
+      log.Println("Failed get!")
+      log.Println(err)
     } else {
-      sort.Sort(ByTime(kibanaRespons.Hits.Hits))
-      // start pairing process.  if first record is VSATs, throw it away
-      messages_only := kibanaRespons.Hits.Hits
-      if strings.Contains(strings.ToLower(messages_only[0].Fields.Event[0]), "vsat") {
-        messages_only = messages_only[1:]
-      }
-      BatsRanges = make([]BatsRange, len(messages_only)/2+1, len(messages_only)/2+1)
-      for idx, v := range messages_only {
-        if idx % 2 == 0 {
-          BatsRanges[idx/2].StartTime = v.Fields.Timestamp[0]
-        } else {
-          BatsRanges[idx/2].EndTime = v.Fields.Timestamp[0]
+      if len(kibanaRespons.Hits.Hits) == 0 {
+        log.Println("No hits!  Eject!")
+      } else {
+        sort.Sort(ByTime(kibanaRespons.Hits.Hits))
+        // start pairing process.  if first record is VSAT, create a BATS record to start the day
+        messages_only := kibanaRespons.Hits.Hits
+        if strings.Contains(strings.ToLower(messages_only[0].Fields.Event[0]), "vsat") {
+          startMoment := Hits{Fields{Timestamp:[]string{strings.Replace(esindexdate, ".", "-", 2)+"T00:00:00Z"}}}
+          messages_only = append([]Hits{startMoment}, messages_only...)
+        }
+        if strings.Contains(strings.ToLower(messages_only[len(messages_only)-1].Fields.Event[0]), "bats") {
+          endMoment := Hits{Fields{Timestamp:[]string{strings.Replace(esindexdate, ".", "-", 2)+"T23:59:59Z"}}}
+          messages_only = append(messages_only, endMoment)
+        }
+        BatsRanges = make([]BatsRange, len(messages_only)/2, len(messages_only)/2)
+        for idx, v := range messages_only {
+          if idx % 2 == 0 {
+            BatsRanges[idx/2].StartTime = v.Fields.Timestamp[0]
+          } else {
+            BatsRanges[idx/2].EndTime = v.Fields.Timestamp[0]
+          }
         }
       }
     }
@@ -98,7 +227,7 @@ func createArchiveFilelist(directory string) (filelist []FileInfoDir) {
   filelist = make([]FileInfoDir, 0, 100)
   files, err := ioutil.ReadDir(directory)
   if err != nil {
-    fmt.Println(err)
+    log.Println(err)
   } else {
     for _, v := range files {
       if v.IsDir() {
@@ -126,15 +255,42 @@ func filterArchiveFiles(batRanges []BatsRange, fileList []FileInfoDir) (filtered
   return filteredList
 }
 
+func writeToLocalBroker(filesToSend []string, rabbitWriter *RabbitChanWriter) {
+  for _, v := range filesToSend {
+    message_data, err := ioutil.ReadFile(v)
+    if err != nil {
+      log.Println("Failed to read file ", v)
+    } else {
+      rabbitWriter.Write(message_data)
+    }
+  }
+}
+
 func main() {
   flag.Parse()
-  batRanges := createBatsTimeline(*shipname)
+  batRanges := createBatsTimeline(*shipname, *esindexdate)
+  for _, g := range batRanges {
+    log.Println(g)
+  }
 
   fileList := createArchiveFilelist(*spool_dir)
   filteredList := filterArchiveFiles(batRanges, fileList)
 
-  fmt.Println("filtered list:")
+
+  var rabbitWriter *RabbitChanWriter
+  for {
+    rabbitWriter = buildChannel()
+    if rabbitWriter != nil {
+      break
+    } else {
+      log.Println("failed to connect to rabbitmq server, nap and try again")
+      time.Sleep(10*time.Second)
+    }
+  }
+
+  writeToLocalBroker(filteredList, rabbitWriter)
+  log.Println("filtered list:")
   for _, v := range filteredList {
-    fmt.Println(v)
+    log.Println(v)
   }
 }
